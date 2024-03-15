e8b754a9e275e7353034a6f8ea1677bbd6c49bb6
Pratik Thapa <pratik.philos@live.com>
2024-03-15 13:28:18 -0400

    removed un-necessary codes

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
 
 func CheckError(e error) {
 	if e != nil {
@@ -11,24 +8,6 @@ func CheckError(e error) {
 	}
 }
 
-type Test struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-}
-
 func main() {
-
-	var test Test
-	test = Test{
-		Name: "Pratik",
-		Age:  31,
-	}
-
-	numberOfKeys := reflect.TypeOf(test).NumField()
-	fmt.Println(numberOfKeys)
-
+	fmt.Println("Hello World")
 }
-
-// make sure the numbers aren't repeated on the same array.
-// make sure the numbers aren't repeated on the different array for up to 5 times.
-// You can only request
