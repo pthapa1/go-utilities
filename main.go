9f3296eec0d2e19682bcfdd4fa578b7364bf6006
Pratik Thapa <pratik.philos@live.com>
2024-12-07 22:04:16 -0500

    file path

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-func CreateFilePath(filePath string) (string, error) {
+func createFilePath(filePath string) (string, error) {
 	projectRoot, err := os.Getwd()
 	if err != nil {
 		return "", err
@@ -19,11 +19,12 @@ func CreateFilePath(filePath string) (string, error) {
 // a user would provide me with a name -f test
 // in the current working dir "./" list all the file paths
 
-func listAllFiles(path ...string) error {
-	initAbsFp, err := CreateFilePath("")
+func listAllFiles(matchFile string, path ...string) ([]string, error) {
+	var result []string
+	initAbsFp, err := createFilePath("")
 	if err != nil {
 		fmt.Println(err)
-		return err
+		return []string{}, err
 	}
 
 	var startPath string
@@ -36,7 +37,7 @@ func listAllFiles(path ...string) error {
 	dirContents, err := os.ReadDir(startPath)
 	if err != nil {
 		fmt.Println(err)
-		return err
+		return []string{}, err
 	}
 
 	for _, val := range dirContents {
@@ -47,28 +48,38 @@ func listAllFiles(path ...string) error {
 			// ensure that the file matches the pattern above
 			for _, ptrn := range filePattern {
 				if strings.Contains(val.Name(), ptrn) {
-					yamlFiles := strings.Split(val.Name(), ptrn)[0]
-					fmt.Println("Split", yamlFiles)
+					yamlFile := strings.Split(val.Name(), ptrn)[0]
+					result = append(result, yamlFile)
+					// if matchFile == yamlFile {
+					// }
+					// lowercase it
+					// remove yaml and yml
+					// find it's path
+					// and return the relative path in result
+					// error saying that we don't have file with matching name error
+					fmt.Println("Split", yamlFile)
 				}
 			}
 		}
 
 		// recurse into subdirectories
 		if val.IsDir() {
-			subDirPath := filepath.Join(startPath, val.Name()) // Use `startPath`
-			err := listAllFiles(subDirPath)                    // Recurse with new path
+			subDirPath := filepath.Join(startPath, val.Name())  // Use `startPath`
+			matches, err := listAllFiles(matchFile, subDirPath) // Recurse with new path
+			result = append(result, matches...)
 			if err != nil {
 				fmt.Println(err)
-				return err
+				return []string{}, err
 			}
 		}
 	}
-	return nil
+	return result, nil
 }
 
 func main() {
-	err := listAllFiles()
+	files, err := listAllFiles("test.yaml")
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println(files)
 }
