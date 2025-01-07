8b292a73f4e0ddbeed657f258de7af36ae85db1b
Pratik Thapa <pthapa1>
2025-01-06 21:39:28 -0500

    compare and convert

--- main.go
+++ main.go
@@ -1,28 +1,76 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 )
 
-func main() {
-	str1 := ".value"
-	str2 := `getValueOf "foo" "bar"`
-
-	res := strings.Split(str2, " ")
-	var key string
-	for i, value := range res {
-		value = strings.ReplaceAll(value, `"`, "")
-		value = strings.ReplaceAll(value, `'`, "")
-		if res[0] == "getValueOf" && i == 1 {
-			key = value
+func marshalToJSON(value interface{}) (interface{}, error) {
+	switch val := value.(type) {
+	case string, bool, int, float64:
+		return val, nil
+	case nil:
+		return nil, nil
+	default:
+		if arr, ok := value.([]interface{}); ok {
+			var jsonArray []string
+			for _, item := range arr {
+				jsonStr, err := json.Marshal(item)
+				if err != nil {
+					return "", err
+				}
+				jsonArray = append(jsonArray, string(jsonStr))
+			}
+			return fmt.Sprintf("[%s]", strings.Join(jsonArray, ",")), nil
 		}
+		jsonStr, err := json.Marshal(value)
+		if err != nil {
+			return "", err
+		}
+		return string(jsonStr), nil
 	}
-	fmt.Println(key)
-
-	fmt.Println(string(str1[0]) == ".")
 }
 
-// if length is zero, return ""
-// if length is not zero && it only has whitespace, return ""
-// if the length is not zero, and !onlyHasEmptySpace, trim whitespace, and return content
+func replaceInPlace(beforeMap, afterMap map[string]interface{}) map[string]interface{} {
+	// loop over the first one
+	for bKey, bValue := range beforeMap {
+		switch bTypeVal := bValue.(type) {
+		case map[string]interface{}:
+			replaceInPlace(bTypeVal, afterMap)
+		case string:
+			if strings.Contains(bTypeVal, ".") {
+				afterMap[bKey] = 26.2
+			}
+		default:
+			fmt.Println("uncovered type")
+		}
+	}
+	return afterMap
+}
+
+func main() {
+	beforeMap := map[string]interface{}{
+		"foo":   "bar",
+		"miles": "{{.distance}}",
+		"age":   "29",
+		"person": map[string]interface{}{
+			"name": "Jane Doe",
+			"age":  "{{.Age}}",
+		},
+	}
+	afterMap := map[string]interface{}{
+		"foo":   "bar",
+		"miles": "26.2",
+		"age":   "30",
+		"person": map[string]interface{}{
+			"name": "Jane Doe",
+			"age":  "50",
+		},
+	}
+
+	modifiedMap := replaceInPlace(beforeMap, afterMap)
+
+	jsonString, _ := marshalToJSON(modifiedMap)
+	fmt.Println(jsonString)
+}
