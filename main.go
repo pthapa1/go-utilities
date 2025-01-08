63caac64209e4309fe6213a5b7ad6d3486937c06
Pratik Thapa <pthapa1>
2025-01-07 21:14:01 -0500

    replace in place

--- main.go
+++ main.go
@@ -1,52 +1,74 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"strings"
 )
 
-func marshalToJSON(value interface{}) (interface{}, error) {
-	switch val := value.(type) {
-	case string, bool, int, float64:
-		return val, nil
-	case nil:
-		return nil, nil
-	default:
-		if arr, ok := value.([]interface{}); ok {
-			var jsonArray []string
-			for _, item := range arr {
-				jsonStr, err := json.Marshal(item)
-				if err != nil {
-					return "", err
-				}
-				jsonArray = append(jsonArray, string(jsonStr))
-			}
-			return fmt.Sprintf("[%s]", strings.Join(jsonArray, ",")), nil
-		}
-		jsonStr, err := json.Marshal(value)
-		if err != nil {
-			return "", err
-		}
-		return string(jsonStr), nil
-	}
-}
+// func marshalToJSON(value interface{}) (interface{}, error) {
+// 	switch val := value.(type) {
+// 	case string, bool, int, float64:
+// 		return val, nil
+// 	case nil:
+// 		return nil, nil
+// 	default:
+// 		if arr, ok := value.([]interface{}); ok {
+// 			var jsonArray []string
+// 			for _, item := range arr {
+// 				jsonStr, err := json.Marshal(item)
+// 				if err != nil {
+// 					return "", err
+// 				}
+// 				jsonArray = append(jsonArray, string(jsonStr))
+// 			}
+// 			return fmt.Sprintf("[%s]", strings.Join(jsonArray, ",")), nil
+// 		}
+// 		jsonStr, err := json.Marshal(value)
+// 		if err != nil {
+// 			return "", err
+// 		}
+// 		return string(jsonStr), nil
+// 	}
+// }
 
-func replaceInPlace(beforeMap, afterMap map[string]interface{}) map[string]interface{} {
-	// loop over the first one
+func replaceInPlace(
+	beforeMap map[string]interface{},
+	afterMap map[string]interface{},
+	parentKey string,
+) map[string]interface{} {
+	cmprt := make(map[string]interface{})
 	for bKey, bValue := range beforeMap {
+		// Construct the current path key
+		currentKey := bKey
+		if parentKey != "" {
+			currentKey = parentKey + " -> " + bKey
+		}
+
 		switch bTypeVal := bValue.(type) {
-		case map[string]interface{}:
-			replaceInPlace(bTypeVal, afterMap)
 		case string:
+			// Example: Check if value contains a "." and modify accordingly
 			if strings.Contains(bTypeVal, ".") {
-				afterMap[bKey] = 26.2
+				cmprt[currentKey] = "modified_value"
+			} else {
+				cmprt[currentKey] = bValue
 			}
+		case map[string]interface{}:
+			// Recurse into nested maps
+			cmprt = mergeMaps(cmprt, replaceInPlace(bTypeVal, afterMap, currentKey))
 		default:
 			fmt.Println("uncovered type")
+			cmprt[currentKey] = bValue
 		}
 	}
-	return afterMap
+	return cmprt
+}
+
+// Helper function to merge two maps
+func mergeMaps(map1, map2 map[string]interface{}) map[string]interface{} {
+	for k, v := range map2 {
+		map1[k] = v
+	}
+	return map1
 }
 
 func main() {
@@ -59,18 +81,35 @@ func main() {
 			"age":  "{{.Age}}",
 		},
 	}
-	afterMap := map[string]interface{}{
-		"foo":   "bar",
-		"miles": "26.2",
-		"age":   "30",
-		"person": map[string]interface{}{
-			"name": "Jane Doe",
-			"age":  "50",
-		},
+
+	afterMap := map[string]interface{}{} // Unused but included for completeness
+	result := replaceInPlace(beforeMap, afterMap, "")
+	for k, v := range result {
+		fmt.Printf("%s: %v\n", k, v)
 	}
-
-	modifiedMap := replaceInPlace(beforeMap, afterMap)
-
-	jsonString, _ := marshalToJSON(modifiedMap)
-	fmt.Println(jsonString)
 }
+
+// func main() {
+// 	beforeMap := map[string]interface{}{
+// 		"foo":   "bar",
+// 		"miles": "{{.distance}}",
+// 		"age":   "29",
+// 		"person": map[string]interface{}{
+// 			"name": "Jane Doe",
+// 			"age":  "{{.Age}}",
+// 		},
+// 	}
+// 	afterMap := map[string]interface{}{
+// 		"foo":   "bar",
+// 		"miles": "26.2",
+// 		"age":   "30",
+// 		"person": map[string]interface{}{
+// 			"name": "Jane Doe",
+// 			"age":  "50",
+// 		},
+// 	}
+// 	modifiedMap := replaceInPlace(beforeMap, afterMap)
+
+// 	jsonString, _ := marshalToJSON(modifiedMap)
+// 	fmt.Println(jsonString)
+// }
