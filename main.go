1773ccfaa92b941e75fd18c7973d39d2e5fb9631
Pratik Thapa <pthapa1>
2025-01-28 19:31:28 -0500

    open url in the default browser of the user....

--- main.go
+++ main.go
@@ -1,12 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	// Create a `DefaultProvider` instance, which implements `ValueProvider`
-	provider := DefaultProvider{}
-	// Pass the provider into the `setValue` function and print the result
-	// The `setValue` function will use `DefaultProvider`'s `GetValue()` method,
-	// which returns "hello there", and concatenate it with " xaaha".
-	fmt.Println(setValue(provider)) // Expected output: "hello there xaaha"
+	_ = OpenURL("https://www.google.com/")
 }
